4f5542266fa6acbb73b259f5c52b6201658ef876
Michal Samuhel <MichalS@kentico.com>
2024-03-28 13:52:32 +0100

    Moving in refactoring from master branch

--- KenticoLicenseUpdateService/LicenseUpdater.cs
+++ KenticoLicenseUpdateService/LicenseUpdater.cs
@@ -1,3 +1,4 @@
+using CMS.Base;
 using CMS.Core;
 using CMS.EventLog;
 using CMS.LicenseProvider;
@@ -71,7 +72,7 @@ namespace KenticoLicenseUpdateService
                 if (splitString.Any())
                 {
                     int index = splitString[3].IndexOf("EXPIRATION:", StringComparison.Ordinal) + 11;
-                    string timeString = splitString[3].Substring(index, 8).Trim();
+                    string timeString = splitString[2].Substring(index + 1, 8).Trim();
                     DateTime expiryDateTime = new DateTime(Convert.ToInt32(timeString.Substring(0, 4)), Convert.ToInt32(timeString.Substring(4, 2)), Convert.ToInt32(timeString.Substring(6, 2)), 0, 0, 0);
                     if (DateTime.Compare(expiryDateTime, nextRunDate) < 0)
                     {
@@ -88,9 +89,16 @@ namespace KenticoLicenseUpdateService
         private string GenerateNewKeys(List<string> generatedKeys, List<LicenseKeyInfo> instanceKeys)
         {
             string errorMessage = null;
+            string resultMessage = $"{generatedKeys.Count} license keys were generated with setting DeleteOldKeys set to {DeleteOldKeys.ToString()}";
 
             for (int i = 0; i < NumberOfKeys; i++)
             {
+
+                int accesControl = i + 1;
+                if (accesControl > instanceKeys.Count)
+                {
+                    return resultMessage;
+                }
                 LicenseKeyInfo key = instanceKeys[i];
                 if (errorMessage == null && Retries != 0)
                 {
@@ -127,7 +135,7 @@ namespace KenticoLicenseUpdateService
                 }
 
             }
-            return $"{generatedKeys.Count} license keys were generated with setting DeleteOldKeys set to {DeleteOldKeys.ToString()}";
+            return resultMessage;
         }
 
         private static string GetLicenseKey(string sn, string domain, int desiredVersion, string userName, out string errorMessage)
@@ -157,28 +165,31 @@ namespace KenticoLicenseUpdateService
         {
             if (task.TaskData != "")
             {
-                string[] parameters = task.TaskData.Split('\n');
+                string[] parameters = task.TaskData.Split(';');
 
                 this.UserName = parameters[0];
                 LicenseKeySerial = parameters[1];
-                int desiredVersion;
-
-                if (!int.TryParse(parameters[2], out desiredVersion))
-                {
-                    DesiredVersion = 0;
-                }
-
-                DesiredVersion = desiredVersion;
-
                 int numberOfKeys;
 
-                if (!int.TryParse(parameters[3], out numberOfKeys))
+                if (!int.TryParse(parameters[2], out numberOfKeys))
                 {
                     NumberOfKeys = instanceKeys.Count;
                 }
+
                 NumberOfKeys = numberOfKeys;
 
-                if (String.Equals(parameters[4], "true", StringComparison.OrdinalIgnoreCase))
+                int desiredVersion;
+
+                if (!int.TryParse(parameters[3], out desiredVersion))
+                {
+                    desiredVersion = int.Parse(CMSVersion.MainVersion);
+                }
+                else
+                {
+                    DesiredVersion = desiredVersion;
+                }
+
+                if (bool.Parse(parameters[4]))
                 {
                     DeleteOldKeys = true;
                 }
